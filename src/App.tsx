ae296e6949d6458fc3d8700eca2c95132376aca8
Cewr <2241610621@qq.com>
2021-07-06 16:24:10 +0800

    feat: 虚拟滚动组件

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,27 @@ import Roll from './components/Roll';
 
 
 function App() {
-  let num = 200;
+  let num = 0;
 
   let str = ''
 
-  while (num--) {
-    str += num
+  while (num < 100) {
+    num++
+    str += num + ','
   }
 
   return (
     <div className="App">
 
       <div className="main">
-        <Roll height={70}>
+        <Roll height={90} noScrolling>
           <div>{str}</div>
+        </Roll>
+      </div>
+
+      <div className="main">
+        <Roll height={90}>
+          <div>13465645</div>
         </Roll>
       </div>
 
--- src/components/Roll/index.tsx
+++ src/components/Roll/index.tsx
@@ -1,63 +1,97 @@
-import { getIn } from '../../utils';
 import React, { ReactElement, useEffect, useState, useRef } from 'react';
-import './index.less';
+import './index.css';
+import { getIn } from '../../utils';
 import cx from 'classnames';
 
 interface Props {
+  height?: number;
+  noScrolling?: boolean;
   className?: string;
   style?: any;
-  height?: number;
+  spacing?: number;
   children: ReactElement;
 }
 
-export default function Roll({ className, style, height, children }: Props): ReactElement {
+export default function Roll({
+  className,
+  style,
+  height = 70,
+  spacing = 20,
+  children,
+
+  noScrolling = false,
+}: Props): ReactElement {
   const [mainHeight, setMainHeight] = useState<number | false>(false);
   const [showBarBg, setShowBarBg] = useState<boolean>(false);
 
   const [barHeight, setBarHeight] = useState<string>('');
   const [barTop, setBarTop] = useState<string>('0%');
 
-  const main = useRef(null);
+  const [contTop, setContTop] = useState<number>(0);
+
   const barBg = useRef(null);
   const bar = useRef(null);
   const container = useRef(null);
   const cont = useRef(null);
 
   useEffect(() => {
-    const containerHeight = getIn(container, ['current', 'clientHeight'], 0);
-    const contHeight = getIn(cont, ['current', 'clientHeight'], 0);
+    if (!noScrolling) return;
+    // @ts-ignore
+    [...(document.getElementsByClassName('no_scrolling') || [])].forEach((item) => {
+      item?.addEventListener?.(
+        'wheel',
+        (e: Event) => {
+          e.preventDefault();
+        },
+        false,
+      );
+    });
+  }, [noScrolling]);
 
-    if (containerHeight >= contHeight) {
-      setShowBarBg(false);
+  useEffect(() => {
+    setContTop(0);
+    setBarTop('0%');
+
+    const contHeight = getIn(cont, ['current', 'clientHeight'], 0);
+    if (height >= contHeight) {
       setMainHeight(contHeight);
+      setShowBarBg(false);
     } else {
       setMainHeight(false);
       setShowBarBg(true);
-      setBarHeight(`${(containerHeight / contHeight) * 100}%`);
+      setBarHeight(`${((height / contHeight) * 100).toFixed(3)}%`);
     }
-  });
+  }, [children, height]);
 
   return (
     <div
-      ref={main}
-      className={cx('roll-main', className)}
-      style={{ ...style, height: mainHeight === false ? height : mainHeight }}
+      ref={container}
+      className={cx('roll-container', className || '', { no_scrolling: noScrolling })}
+      style={{ ...style, height: mainHeight || height }}
     >
       <div className="roll-bar-bg" ref={barBg} style={{ display: showBarBg ? 'block' : 'none' }}>
         <div className="roll-bar" ref={bar} style={{ height: barHeight, top: barTop }} />
       </div>
-      <div className="roll-container" ref={container}>
-        <div
-          className="roll-cont"
-          ref={cont}
-          onWheel={() => {
-            const contHeight = getIn(cont, ['current', 'clientHeight'], 0);
-            const scrollTop = getIn(container, ['current', 'scrollTop'], 0);
-            setBarTop(`${(scrollTop / contHeight) * 100}%`);
-          }}
-        >
-          {children}
-        </div>
+
+      <div
+        className="roll-cont"
+        ref={cont}
+        style={{ top: contTop }}
+        onWheel={(e) => {
+          if (!showBarBg) return;
+          const contHeight = getIn(cont, ['current', 'clientHeight'], 0);
+          const containerHeight = getIn(container, ['current', 'clientHeight'], 0);
+
+          const x = contTop - (e?.deltaY > 0 ? spacing : -spacing);
+          const x2 = `${((-x / contHeight) * 100).toFixed(3)}%`;
+
+          if (x < -contHeight + containerHeight - spacing || x > 0) return;
+
+          setContTop(x);
+          setBarTop(x2);
+        }}
+      >
+        {children}
       </div>
     </div>
   );
